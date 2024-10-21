1b07c81f951b66c8caa30568e644bba1e73b11fa
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2024-10-21 17:23:18 -0400

    feat: added homwork of 21-10-2024

---
+++ src/test/java/testSuiteTodoist/LoginTest.java
@@ -0,0 +1,15 @@
+package testSuiteTodoist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LoginTest extends BaseTestTodoist{
+    @Test
+    public void verifyLogin(){
+        mainPageTodoist.loginButton.click();
+        loginPageTodoist.emailTextBox.setText("apazahuaychohugoalejandro@gmail.com");
+        loginPageTodoist.passwordTextBox.setText("Holasoyyo200222222");
+        loginPageTodoist.loginButton.click();
+        Assertions.assertTrue(menuSectionTodoist.profileButton.isControlDislayed(), "Error: lofin failed");
+    }
+}
---
+++ src/test/java/todoLy/UpdateFullName.java
@@ -0,0 +1,29 @@
+package todoLy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.todoly.MenuSection;
+import pages.todoly.SettingsSection;
+import testSuite.BaseTestTodoLy;
+
+public class UpdateFullName extends BaseTestTodoLy {
+
+    MenuSection menuSection = new MenuSection();
+    SettingsSection settingsSection = new SettingsSection();
+
+    @Test
+    public void verifyUpdateFullName() throws InterruptedException {
+        mainPage.loginButton.click();
+        loginSection.login("apazahuaychohugoalejandro@gmail.com","Holasoyyo2002");
+        String name = "Hugo Test 1234";
+        menuSection.settingsButton.click();
+        settingsSection.fullNameTextbox.clearSetText(name);
+        settingsSection.okButton.click();
+        Thread.sleep(10000);
+        menuSection.settingsButton.click();
+        Thread.sleep(5000);
+        String actualResult = settingsSection.fullNameTextbox.getValue();
+        Assertions.assertEquals(name,actualResult,
+                "ERROR the Full Name was not updated");
+    }
+}
